766f8510f066d9ba26accf3fad8a40b7789bad25
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2018-06-14 15:22:02 -0400

    refactor code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 /*
  * Create a list that holds all of your cards
  */
-let cardHolder = [];
+let cardHolder = ['fa-diamond', 'fa-diamond',
+                  'fa-paper-plane-o', 'fa-paper-plane-o',
+                  'fa-anchor', 'fa-anchor',
+                  'fa-bolt', 'fa-bolt',
+                  'fa-cube', 'fa-cube',
+                  'fa-leaf', 'fa-leaf',
+                  'fa-bicycle', 'fa-bicycle',
+                  'fa-bomb', 'fa-bomb'
+              ];
+
 const score_panel = document.querySelector('.score-panel');
 const stars = document.querySelector('.stars');
 const moves = document.querySelector('.moves');
 const restart = document.querySelector('.restart');
 const deck = document.querySelector('.deck');
 const cards = document.querySelectorAll('.card');
+const openCards = [];
 
 
 /*
@@ -16,14 +26,14 @@ const cards = document.querySelectorAll('.card');
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-function displayCards() {
-  // console.log(cards)
-  cards.forEach(function(card){
-     card.addEventListener('click', function(e){
-      card.classList.add('open', 'show');
-    })
-  })
-}
+// function displayCards() {
+//   // console.log(cards)
+//   cards.forEach(function(card){
+//      card.addEventListener('click', function(e){
+//       card.classList.add('open', 'show');
+//     })
+//   })
+// }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -61,8 +71,26 @@ function shuffle(array) {
 // }
 
 
+cards.forEach((card) => {
+  card.addEventListener('click', (e) => {
+    card.classList.add('open', 'show');
+    //.classList will retrieve the list of classes set on the card element
+    // .add method is letting me add .open and .show class to the element
+    // read for more: https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList
+
+    openCards.push(card);
+    console.log('openCards', openCards.length)
 
 
+     if(openCards.length >= 2) {
+      setTimeout( () => {
+        openCards.forEach( (card) => {
+          card.classList.remove('open', 'show');
+          })
+        }, 1000)
+      }
+  })
+})
 
 
 
